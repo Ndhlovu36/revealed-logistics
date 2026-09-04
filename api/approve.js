9c66677df5be27c1ac3c7282278cdacd75c14548
Ndhlovu36 <dumandhlovu361@gmail.com>
2026-09-04 22:24:53 +0200

    Delete api/approve.js

--- api/approve.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req,res){
- const {paymentId} = req.body;
- const r = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`,{
-  method:'POST',
-  headers:{'Authorization':`Key ${process.env.PI_API_KEY}`}
- });
- const d = await r.json();
- res.status(200).json(d);
-}
