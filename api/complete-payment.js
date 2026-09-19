3c1b1083a304ca2ddf4a43115fddc70093535257
Ndhlovu36 <dumandhlovu361@gmail.com>
2026-09-19 04:46:26 +0200

    Delete api/complete-payment.js

--- api/complete-payment.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req,res){
- const {paymentId, txid}=req.body;
- try{
-  const r=await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`,{
-   method:'POST', headers:{Authorization:`Key ${process.env.PI_API_KEY}`, 'Content-Type':'application/json'}, body:JSON.stringify({txid})
-  });
-  const d=await r.json(); return res.status(200).json(d);
- }catch(e){ return res.status(200).json({completed:true}); }
-}
